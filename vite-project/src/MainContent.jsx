6b2e1eb5b674588603bf337053e44a2607a2e2a4
rachitjaiswal <rachitjaiswal999@gmail.com>
2024-06-05 19:25:03 +0530

    i

--- vite-project/src/MainContent.jsx
+++ vite-project/src/MainContent.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './MainContent.css';
+import { FaHeart, FaComment, FaShare } from 'react-icons/fa';
 
 function MainContent() {
   return (
     <div className="main-content">
       <div className="post">
         <div className="user">
-          <img src= "./src/assets/pexels-ali-pazani-2613260@2x.png" alt="user" className="user-image" />
+          <img src="./src/assets/pexels-ali-pazani-2613260@2x.png" alt="user" className="user-image" />
           <div>
             <p className="username">Lara Leones</p>
             <p className="user-handle">@thewallart</p>
@@ -17,14 +18,14 @@ function MainContent() {
         </p>
         <img src="./src/assets/pexels-humphrey-muleba-2045248@2x.png" alt="post" className="post-image" />
         <div className="post-stats">
-          <span>9.8k</span>
-          <span>8.6k</span>
-          <span>7.2k</span>
+          <span><FaHeart className="icon" /> 9.8k</span>
+          <span><FaComment className="icon" /> 8.6k</span>
+          <span><FaShare className="icon" /> 7.2k</span>
         </div>
       </div>
       <div className="post">
         <div className="user">
-          <img src= "./src/assets/pexels-imad-clicks-9810659@2x.png" alt="user" className="user-image" />
+          <img src="./src/assets/pexels-imad-clicks-9810659@2x.png" alt="user" className="user-image" />
           <div>
             <p className="username">Thomas J.</p>
             <p className="user-handle">@thecustomcreator</p>
@@ -35,9 +36,9 @@ function MainContent() {
         </p>
         <img src="./src/assets/pexels-tobias-bjørkli-2236382@2x.png" alt="post" className="post-image" />
         <div className="post-stats">
-          <span>9.8k</span>
-          <span>8.6k</span>
-          <span>7.2k</span>
+          <span><FaHeart className="icon" /> 9.8k</span>
+          <span><FaComment className="icon" /> 8.6k</span>
+          <span><FaShare className="icon" /> 7.2k</span>
         </div>
       </div>
       {/* Repeat the post div for more posts */}
